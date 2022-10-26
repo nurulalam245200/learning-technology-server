50aa310e9c7fff4519704ddd09b67febf51186d0
nurulalam <jack245200@gmail.com>
2022-10-27 05:06:04 +0600

    courses data create and category name modify

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const categories = require("./data/categories.json");
 app.get("/", (req, res) => {
   res.send("Learning App Rurring");
+});
+
+app.get("/category/:id", (req, res) => {
+  res.send(categories);
+  const id = req.params.id;
+  if (id == "8") {
+    res.send(categories);
+  } else {
+    const courses_category = categories.filter(
+      (category) => category.category_Id === id
+    );
+    res.send(courses_category);
+  }
 });
 app.listen(port, () => {
   console.log("Server running on", port);
