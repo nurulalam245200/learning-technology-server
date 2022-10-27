22cec45d7b1b9a507418c16203dcb078b7d2db26
nurulalam <jack245200@gmail.com>
2022-10-27 06:07:02 +0600

    all error issues solve in index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 
 const categories = require("./data/categories.json");
+const courses = require("./data/courses.json");
 
 app.use(cors());
 
@@ -11,18 +12,31 @@ app.get("/", (req, res) => {
   res.send("Learning App Rurring");
 });
 
-app.get("/category/:id", (req, res) => {
+app.get("/category", (req, res) => {
   res.send(categories);
+});
+
+app.get("/category/:id", (req, res) => {
   const id = req.params.id;
-  if (id == "8") {
-    res.send(categories);
+  if (id === "08") {
+    res.send(courses);
   } else {
-    const courses_category = categories.filter(
-      (category) => category.category_Id === id
-    );
-    res.send(courses_category);
+    const c_category = courses.filter((e) => e.category_Id === id);
+    res.send(c_category);
   }
 });
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const selected_course = courses.find((course) => course.id === id);
+
+  res.send(selected_course);
+});
+
+app.get("/course", (req, res) => {
+  res.send(courses);
+});
+
 app.listen(port, () => {
   console.log("Server running on", port);
 });
